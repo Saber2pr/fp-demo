e51e4b4ff9f9467ae1b6bbfeb5cf3d7c37878a4a
saber2pr <saber2pr@gmail.com>
2019-03-12 00:32:34 +0800

    update

--- src/demo/test0.js
+++ src/demo/test0.js
@@ -2,7 +2,7 @@
  * @Author: saber2pr
  * @Date: 2019-02-11 18:23:53
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-03-12 00:27:10
+ * @Last Modified time: 2019-03-12 00:32:17
  */
 const connect = s => p =>
   connect(p(s))
